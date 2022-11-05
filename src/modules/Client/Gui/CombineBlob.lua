88ad5b84f968c09a2d923d03af9a7c962083d24b
Chabba100 <ourgoodminecraftrlserver@outlook.com>
2022-11-05 16:48:23 -0700

    basic combining ui

--- src/modules/Client/Gui/CombineBlob.lua
+++ src/modules/Client/Gui/CombineBlob.lua
@@ -3,7 +3,7 @@ local require = require(script.Parent.loader).load(script)
 local BasicPane = require("BasicPane")
 local Blend = require("Blend")
 local BasicPaneUtils = require("BasicPaneUtils")
-local ValueObject = require("ValueObject")
+local RxBrioUtils = require("RxBrioUtils")
 
 local CombineBlob = setmetatable({}, BasicPane)
 CombineBlob.ClassName = "CombineBlob"
@@ -25,7 +25,7 @@ end
 function CombineBlob:_render()
     local percentVisible = Blend.Spring(BasicPaneUtils.observePercentVisible(self), 30)
     local transparency = BasicPaneUtils.toTransparency(percentVisible)
-    
+
     return Blend.New "BillboardGui" {
         Name = "CombineBasket";
         Adornee = self._adornee;
@@ -36,21 +36,26 @@ function CombineBlob:_render()
                 Size = UDim2.fromScale(1, 1);
 
                 [Blend.Children] = {
+                    self._fruitList:ObserveItemsBrio():Pipe({
+                        RxBrioUtils.map(function(fruitName)
+                            print(fruitName)
+                            return Blend.New "ImageButton" {
+                                Name = fruitName;
+                                Size = UDim2.fromScale(0.2, 1);
+                                BackgroundColor3 = Color3.fromRGB(182, 21, 21);
+
+                                [Blend.Children] = {
+                                    Blend.New "UIAspectRatioConstraint" {
+                                        AspectRatio = 1;
+                                    }
+                                }
+                            }
+                        end)
+                    }),
                     Blend.New "UIListLayout" {
                         FillDirection = Enum.FillDirection.Horizontal;
-                    };
-                    Blend.ComputedPairs(self._fruitList:ObserveItemsBrio(), function(_index, brio)
-                        if brio:IsDead() then
-                            return
-                        end
-
-                        local fruit = brio:GetValue()
-
-                        print("running", fruit)
-                        return Blend.New "ImageButton" {
-                            Name = fruit;
-                        }
-                    end)
+                        --Padding = UDim.new(0, 5)
+                    }
                 }
             }
         }
